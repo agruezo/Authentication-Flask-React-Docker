e9edcb1fdd763f63c8d6a29a1058d8c293d10163
agruezo <agruezo@gmail.com>
2022-11-12 13:12:43 -0500

    added CI

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
 
-import UsersList from '../UsersList';
+import UsersList from "../UsersList";
 
 afterEach(cleanup);
 
 const users = [
-    {
-        "email": "redninja@dojo.com",
-        "id": 1,
-        "username": "redninja"
-    },
-    {
-        "email": "blueninja@dojo.com",
-        "id": 2,
-        "username": "blueninja"
-    }
+  {
+    email: "redninja@dojo.com",
+    id: 1,
+    username: "redninja",
+  },
+  {
+    email: "blueninja@dojo.com",
+    id: 2,
+    username: "blueninja",
+  },
 ];
 
 it("renders a username", () => {
-    const { getByText } = render(<UsersList users={users} />);
-    expect(getByText("redninja")).toHaveClass("username");
-    expect(getByText("blueninja")).toHaveClass("username");
+  const { getByText } = render(<UsersList users={users} />);
+  expect(getByText("redninja")).toHaveClass("username");
+  expect(getByText("blueninja")).toHaveClass("username");
 });
-it('renders', () => {
-    const { asFragment } = render(<UsersList users={users} />);
-    expect(asFragment()).toMatchSnapshot();
-}); 
+it("renders", () => {
+  const { asFragment } = render(<UsersList users={users} />);
+  expect(asFragment()).toMatchSnapshot();
+});
