8832305c2995e2987f49f18deb8a40f6284448aa
agruezo <agruezo@gmail.com>
2022-11-17 11:48:23 -0500

    refactored UserList and AddUser components

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
+import Modal from "react-modal";
 
 import UsersList from "./components/UsersList";
-import AddUser from "./components/AddUser";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
 import UserStatus from "./components/UserStatus";
 import Message from "./components/Message";
+import AddUser from "./components/AddUser";
+
+const modalStyles = {
+  content: {
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    border: 0,
+    background: "transparent",
+  },
+};
+
+Modal.setAppElement(document.getElementById("root"));
 
 class App extends Component {
   constructor() {
@@ -21,10 +35,8 @@ class App extends Component {
       accessToken: null,
       messageType: null,
       messageText: null,
+      showModal: false,
     };
-    // this.addUser = this.addUser.bind(this); #use this only if an arrow function isn't used for addUser method below
-
-    // this.handleChange = this.handleChange.bind(this); #use this only if an arrow function isn't used for handleChange method below
   }
 
   componentDidMount() {
@@ -41,10 +53,12 @@ class App extends Component {
           username: "",
           email: "",
         });
+        this.handleCloseModal();
         this.createMessage("success", "User added");
       })
       .catch((err) => {
         console.log(err);
+        this.handleCloseModal();
         this.createMessage("danger", "That user already exists.");
       });
   };
@@ -149,6 +163,31 @@ class App extends Component {
     });
   };
 
+  handleOpenModal = () => {
+    this.setState({
+      showModal: true,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  removeUser = (user_id) => {
+    axios
+      .delete(`${process.env.REACT_APP_API_SERVICE_URL}/users/${user_id}`)
+      .then((res) => {
+        this.getUsers();
+        this.createMessage("success", "User removed.");
+      })
+      .catch((err) => {
+        console.log(err);
+        this.createMessage("danger", "Something went wrong.");
+      });
+  };
+
   render() {
     return (
       <div>
@@ -178,16 +217,42 @@ class App extends Component {
                         <h1 className="title is-1 is-1">Users</h1>
                         <hr />
                         <br />
-                        <AddUser
-                          username={this.state.username}
-                          email={this.state.email}
-                          addUser={this.addUser}
-                          // eslint-disable-next-line react/jsx-handler-names
-                          handleChange={this.handleChange}
+                        {this.isAuthenticated() && (
+                          <button
+                            onClick={this.handleOpenModal}
+                            className="button is-primary"
+                          >
+                            Add User
+                          </button>
+                        )}
+                        <br />
+                        <br />
+                        <Modal
+                          isOpen={this.state.showModal}
+                          style={modalStyles}
+                        >
+                          <div className="modal is-active">
+                            <div className="modal-background" />
+                            <div className="modal-card">
+                              <header className="modal-card-head">
+                                <p className="modal-card-title">Add User</p>
+                                <button
+                                  className="delete"
+                                  aria-label="close"
+                                  onClick={this.handleCloseModal}
+                                />
+                              </header>
+                              <section className="modal-card-body">
+                                <AddUser addUser={this.addUser} />
+                              </section>
+                            </div>
+                          </div>
+                        </Modal>
+                        <UsersList
+                          users={this.state.users}
+                          removeUser={this.removeUser}
+                          isAuthenticated={this.isAuthenticated}
                         />
-                        <br />
-                        <br />
-                        <UsersList users={this.state.users} />
                       </div>
                     }
                   />
--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -20,11 +20,35 @@ const users = [
 ];
 
 it("renders a username", () => {
-  const { getByText } = render(<UsersList users={users} />);
+  const { getByText } = render(
+    <UsersList
+      users={users}
+      removeUser={() => true}
+      isAuthenticated={() => true}
+    />
+  );
   expect(getByText("redninja")).toHaveClass("username");
   expect(getByText("blueninja")).toHaveClass("username");
 });
+
 it("renders", () => {
-  const { asFragment } = render(<UsersList users={users} />);
+  const { asFragment } = render(
+    <UsersList
+      users={users}
+      removeUser={() => true}
+      isAuthenticated={() => false}
+    />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders when authenticated", () => {
+  const { asFragment } = render(
+    <UsersList
+      users={users}
+      removeUser={() => true}
+      isAuthenticated={() => true}
+    />
+  );
   expect(asFragment()).toMatchSnapshot();
 });
